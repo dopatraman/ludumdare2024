1f760f5e5d766b8905c82867d81807e8bcb49a1e
Cameron Williams <williamscamr@gmail.com>
2024-10-06 13:58:36 -0700

    "changes: - main menu music added - added sound effects to repo - changed game music to audiostreamplayer instead of audiostreamplayer2d node, because it is nonpositional"

--- start_screen.gd
+++ start_screen.gd
@@ -3,7 +3,7 @@ signal start_game
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$MainMenuMusic.play()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -23,9 +23,12 @@ func show_game_over():
 	await $MessageTimer.timeout
 
 	$Message.text = "Pizza Rat"
-	$Message.show()
+	#$Message.show()
 	# Make a one-shot timer and wait for it to finish.
 	await get_tree().create_timer(1.0).timeout
+	$MainBackground.show()
+	$Logo.show()
+	$MainMenuMusic.play()
 	$StartButton.show()
 
 
@@ -35,4 +38,7 @@ func _on_message_timer_timeout() -> void:
 
 func _on_start_button_pressed() -> void:
 	$StartButton.hide()
+	$MainMenuMusic.stop()
+	$MainBackground.hide()
+	$Logo.hide()
 	start_game.emit()
