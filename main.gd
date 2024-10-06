5b2c96ca695d0c452d0cf3f00540896fa170678c
Prakash Venkatraman <venkatraman.prakash@gmail.com>
2024-10-05 20:18:15 -0700

    roaches

--- main.gd
+++ main.gd
@@ -1,5 +1,6 @@
 extends Node
 
+@export var roach_scene: PackedScene
 var score
 
 # Called when the node enters the scene tree for the first time.
@@ -20,7 +21,34 @@ func new_game():
 	$StartPosition.position = get_viewport().size / 2
 	$Rat.start($StartPosition.position)
 	$StartScreen.show_message("Get Ready!")
+	$RoachTimer.start()
+	get_tree().call_group("roaches", "queue_free")
 
 
 func _on_start_screen_start_game() -> void:
 	new_game()
+
+
+func _on_roach_timer_timeout() -> void:
+	var roach = roach_scene.instantiate()
+
+	# Choose a random location on Path2D.
+	var roach_spawn_location = $RoachPath/RoachSpawnLocation
+	roach_spawn_location.progress_ratio = randf()
+
+	# Set the roach's direction perpendicular to the path direction.
+	var direction = roach_spawn_location.rotation + PI / 2
+
+	# Set the roach's position to a random location.
+	roach.position = roach_spawn_location.position
+
+	# Add some randomness to the direction.
+	direction += randf_range(-PI / 4, PI / 4)
+	roach.rotation = direction
+
+	# Choose the velocity for the roach.
+	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
+	roach.linear_velocity = velocity.rotated(direction)
+
+	# Spawn the roach by adding it to the Main scene.
+	add_child(roach)
--- rat.gd
+++ rat.gd
@@ -3,14 +3,20 @@ signal death
 
 @export var speed = 800 # How fast the player will move (pixels/sec).
 var screen_size # Size of the game window.
-
+var is_dead = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	is_dead = false
 	hide()
 	screen_size = get_viewport_rect().size
+	$AnimatedSprite2D.animation = "idle"
+	$AnimatedSprite2D.play()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if is_dead:
+		return
+
 	var velocity = Vector2.ZERO # The player's movement vector.
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
@@ -52,19 +58,25 @@ func _process(delta):
 
 
 func _on_body_entered(body: Node2D) -> void:
+	is_dead = true
 	$AnimatedSprite2D.animation = "death"
 	$AnimatedSprite2D.play()
+	death.emit()
 	
 	# Must be deferred as we can't change physics properties on a physics callback.
 	$CollisionShape2D.set_deferred("disabled", true)
+	await get_tree().create_timer(0.5).timeout
+	$AnimatedSprite2D.stop()
+	hide()
 
 
-func _on_animated_sprite_2d_animation_finished() -> void:
-	if $AnimatedSprite2D.animation == 'death':
-		death.emit()
+#func _on_animated_sprite_2d_animation_finished() -> void:
+	#if $AnimatedSprite2D.animation == 'death':
+		#death.emit()
 
 
 func start(pos):
 	position = pos
 	show()
 	$CollisionShape2D.disabled = false
+	is_dead = false
